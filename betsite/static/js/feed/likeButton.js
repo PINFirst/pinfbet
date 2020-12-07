ea517de1fff4e9d636c243b5ffeca72c6dec45a2
jeffresh <manuel.pallanunez@alum.uca.es>
2020-12-07 20:34:04 +0100

    feat: add dynamic behavior to like button

--- betsite/static/js/feed/likeButton.js
+++ betsite/static/js/feed/likeButton.js
@@ -1,4 +1,4 @@
-function changeLikeColor() {
-    const likeBtn =  document.getElementById("likeBtn");
+function changeLikeColor(index) {
+    const likeBtn =  document.getElementById("likeBtn-" + index );
     likeBtn.style.color = likeBtn.style.color === "black" ? "orange" : "black";
 }
