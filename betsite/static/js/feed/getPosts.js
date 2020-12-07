0d94456a2b1addc535cf0a86f3c14e524feb1aa0
jeffresh <manuel.pallanunez@alum.uca.es>
2020-12-07 20:13:54 +0100

    refactor: update feed
    move function getPost to his own file

--- betsite/static/js/feed/getPosts.js
+++ betsite/static/js/feed/getPosts.js
@@ -1,5 +1,23 @@
-async function getPosts() {
-    let response = await fetch("get_posts")
-    let data = await response.json()
-    console.log(data)
-}
+
+
+(
+    async function getPosts() {
+        try {
+            let response = await fetch("get_posts")
+            if (!response.ok)
+                throw {
+                    status: response.status,
+                    statusText: response.statusText
+                }
+            let data = await response.json()
+            console.log(data)
+            createPost(data)
+
+        } catch (e) {
+            console.log(e)
+
+        } finally {
+
+        }
+    }
+)();
