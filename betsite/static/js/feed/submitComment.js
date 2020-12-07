983a40cdfdce3930d0f55e6a73d2dc6ae27d47c4
jeffresh <manuel.pallanunez@alum.uca.es>
2020-12-07 18:48:54 +0100

    refactor: change submit comments
    now using vanilla javascript

--- betsite/static/js/feed/submitComment.js
+++ betsite/static/js/feed/submitComment.js
@@ -1,24 +1,71 @@
-function addComment() {
+const submitComment = () => {
+    // const $fetch = document.getElementById("comment-message")
+    addComment()
+};
 
-    let message = $("#comment-message")
+function getMessageValue() {
+    return new Promise((resolve, reject) => {
+        resolve({
+            result: $("#comment-message").val()
+        })
+    });
+}
 
 
-    let comment = "<div id='comment' class='d-flex flex-row align-items-center feed-text px-2'>\n" +
-        "                <div class=\"circle-img img-circle rounded-circle justify-content-start\">\n" +
-        "                    <img id=\"comment-user-img\"\n" +
-        "                         src=\"https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg\" alt=\"user-image\">\n" +
-        "                </div>\n" +
-        "                <div class=\"d-flex flex-column flex-wrap ml-2\">\n" +
-        "                    <span id=\"comment-userName\" class=\"font-weight-bold\">@Username</span>\n" +
-        "                    <span id=\"comment-time\" class=\"text-black-50 time\">35 minutes ago</span>\n" +
-        "                    <span id=\"comment-message\"><b>Comentó: </b>" + message.val() + "</span>\n" +
-        "                </div>\n" +
-        "\n" +
-        "                <div class=\"feed-icon ml-auto p-2\"><i class=\"fa fa-ellipsis-v text-black-50\"></i></div>\n" +
-        "            </div>"
+async function addComment() {
 
-    message.val("")
-    $("#comments").append(comment)
+    let message = await getMessageValue()
 
+    let divComment = document.createElement('div')
+    divComment.setAttribute('class', 'd-flex flex-row align-items-center feed-text px-2')
+    divComment.id = 'comment'
+
+    let divImg = document.createElement('div')
+    divImg.setAttribute('class', 'circle-img img-circle rounded-circle justify-content-start')
+    divImg.setAttribute('class', 'circle-img img-circle rounded-circle justify-content-start')
+    divImg.id = 'comment-profile-img'
+
+
+    let imgProfile = document.createElement('img')
+    imgProfile.setAttribute('src', 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg')
+    imgProfile.setAttribute('alt', 'user-image')
+
+    divImg.appendChild(imgProfile)
+
+    divComment.appendChild(divImg)
+
+
+
+    let divContent = document.createElement('div')
+    divContent.setAttribute('class', 'd-flex flex-column flex-wrap ml-2')
+    divContent.id = 'content'
+
+    divComment.appendChild(divContent)
+
+    let spanContentUserName = document.createElement('span')
+    spanContentUserName.setAttribute('class', 'font-weight-bold')
+    spanContentUserName.innerText = "@Username"
+    spanContentUserName.id = 'comment-userName'
+
+    let spanContentTime = document.createElement('span')
+    spanContentTime.setAttribute('class', 'text-black-50 time')
+    spanContentTime.innerText = "35 minutes ago"
+    spanContentTime.id = 'comment-time'
+
+    let spanContentMessage = document.createElement('span')
+    spanContentMessage.setAttribute('class', 'font-weight-bold')
+    spanContentMessage.innerText = "Comentó: " + message.result
+    spanContentMessage.id = 'comment-message'
+
+    divContent.appendChild(spanContentUserName)
+    divContent.appendChild(spanContentTime)
+    divContent.appendChild(spanContentMessage)
+
+    let optionsIcon = document.createElement('i')
+    optionsIcon.setAttribute('class', 'fa fa-ellipsis-v text-black-50 feed-icon ml-auto p-2')
+
+    divComment.appendChild(optionsIcon)
+
+    document.getElementById('comments').appendChild(divComment)
 
 }
