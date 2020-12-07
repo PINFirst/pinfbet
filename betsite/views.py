61473f4245f6e0d6d0970b9f52542215acc65ffe
jeffresh <manuel.pallanunez@alum.uca.es>
2020-12-07 20:10:38 +0100

    feat: update get post view
    add more mock comments

--- betsite/views.py
+++ betsite/views.py
@@ -35,8 +35,55 @@ class GetPosts(View):
               'profile_img': 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg',
               'message': 'Pfff'
               },
+
          ]
-         }
+         },
+        {'User': 'Maria',
+         'profile_img': 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg',
+         'message': 'Me quiero pegar un tiro',
+         'type': 'bet',
+         'bet': 'Ha apostado: Apruebo PINF',
+         'image': None,
+         'image_url': None,
+         'comments': [
+             {'User': 'Sergio',
+              'profile_img': 'https://images.pexels.com/photos/1998456/pexels-photo-1998456.jpeg',
+              'message': 'Vamos a sacar un diez'
+              },
+             {'User': 'Sergio',
+              'profile_img': 'https://images.pexels.com/photos/1998456/pexels-photo-1998456.jpeg',
+              'message': 'Era broma'
+              },
+             {'User': 'Jeff',
+              'profile_img': 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg',
+              'message': 'Pfff'
+              },
+
+         ]
+         },
+        {'User': 'Sergio',
+         'profile_img': 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg',
+         'message': 'Me quiero pegar un tiro',
+         'type': 'bet',
+         'bet': 'Ha apostado: Apruebo PINF',
+         'image': None,
+         'image_url': None,
+         'comments': [
+             {'User': 'Sergio',
+              'profile_img': 'https://images.pexels.com/photos/1998456/pexels-photo-1998456.jpeg',
+              'message': 'Vamos a sacar un diez'
+              },
+             {'User': 'Sergio',
+              'profile_img': 'https://images.pexels.com/photos/1998456/pexels-photo-1998456.jpeg',
+              'message': 'Era broma'
+              },
+             {'User': 'Jeff',
+              'profile_img': 'https://images.pexels.com/photos/1370750/pexels-photo-1370750.jpeg',
+              'message': 'Pfff'
+              },
+
+         ]
+         },
     ]}
 
     def get(self, request):
