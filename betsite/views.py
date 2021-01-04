249c3771bac353e89888c988439f607030349313
SergioDR <sergio.diazra@alum.uca.es>
2021-01-04 19:40:04 +0100

    feat: add login validation

--- betsite/views.py
+++ betsite/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 class Feed(View):
     template = 'feed.html'
@@ -10,8 +11,18 @@ class Feed(View):
     def get(self, request):
         return render(request, self.template)
 
-class Login(View):
-    template = 'login.html'
 
-    def get(self, request):
-        return render(request, self.template)
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('feed')
+        else:
+            messages.info(request, 'Usuario o contraseña incorrectas')
+            print('Usuario incorrecto')
+
+    return render(request, 'login.html')
--- pinfbetsite/urls.py
+++ pinfbetsite/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from betsite import views
+from betsite import views as betsite_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feed/', views.Feed.as_view(), name='feed'),
-    path('login/', views.Login.as_view(), name='login')
+    path('feed/', betsite_views.Feed.as_view(), name='feed'),
+    path('', betsite_views.loginPage, name='login')
+    #path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login')
 ]
