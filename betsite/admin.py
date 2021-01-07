8d3e26b7706bfb5c00f700263a7c4a970f4af068
SergioDR <sergio.diazra@alum.uca.es>
2021-01-07 19:03:05 +0100

    feat: register Bet model

--- betsite/admin.py
+++ betsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Student, Subject
+from .models import Student, Subject, Bet, Grade
 
 admin.site.register(Student)
 admin.site.register(Subject)
+admin.site.register(Bet)
