0ca4ca8f9cbb909383d913a378b952e468091865
Maria Victoria Alba <mariavictoria.albacarav@alum.uca.es>
2021-01-08 22:43:18 +0100

    js for SingUp

--- betsite/static/js/SignUp.js
+++ betsite/static/js/SignUp.js
@@ -1,81 +1,60 @@
-function revisar(elemento){
-    if(elemento.value==''){ //si el campo está vacío
-        elemento.className='error';
-    }else{
-        elemento.className='input';
-    }
-}
-
-function revisarNombre(elemento){
-    if(elemento.value!==''){
-        var data = elemento.value;
-        var exp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/; //expresión para nombre simple o compuesto
-        if(!exp.test(data)){
-            elemento.className='error';
-        }else{
-            elemento.className='input';
-        }
-    }
-}
-function revisarApellidos(elemento){
-    if(elemento.value!==''){
-        var data = elemento.value;
-        var exp = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/; //expresión para uno o varios apellidos
-        if(!exp.test(data)){
-            elemento.className='error';
-        }else{
-            elemento.className='input';
-        }
-    }
-}
-function revisarEmail(elemento){
-    if(elemento.value!==''){
-        var data = elemento.value
-        var exp = /\w+@\w+\.+[a-z]/ //expresión de email
-        if(!exp.test(data)){ //si no cumple la expresión
-            elemento.className='error';
-        }else{
-            elemento.className='input';
-        }
-    }
-}
-
-function validar()
+function validate()
 {
-    var nombre,apellidos,correo,expresionnombre,expresionemail;
-    nombre = document.getElementById("nombre").value;
-    apellidos = document.getElementById("apellidos").value;
-    correo = document.getElementById("correo").value;
-    expresionemail = /\w+@\w+\.+[a-z]/;
-    expresionnombre = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/;
+    var name,surnames,email,user,nameexpression,emailexpression,password1,password2;
+    name = document.getElementById("id_name").value;
+    surnames = document.getElementById("id_surnames").value;
+    email = document.getElementById("id_email").value;
+    password1 = document.getElementById("id_password").value;
+    password2 = document.getElementById("id_confirm").value;
+    user = document.getElementById("id_username").value;
+    emailexpression = /[a-z]+\.[a-z]+@alum\.uca\.es/;
+    nameexpression = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s[a-zA-ZÀ-ÿ\u00f1\u00d1]+)*$/;
 
-    if(nombre === "" || !expresionnombre.test(nombre)){
-        document.getElementById("nombre").className ='error';
-        document.getElementById("nombre").value  = "";
+    if(name === "" || !nameexpression.test(name)){
+        document.getElementById("id_name").className ='error';
+        document.getElementById("id_name").value  = "";
         return false;
     }
-    else if(apellidos === "" || !expresionnombre.test(apellidos)){
-        document.getElementById("apellidos").className='error';
-        document.getElementById("apellidos").value  = "";
-        return false;
+    else{
+        document.getElementById("id_name").className ='input';
+        if(surnames === "" || !nameexpression.test(surnames)){
+            document.getElementById("id_surnames").className='error';
+            document.getElementById("id_surnames").value  = "";
+            return false;
+        }
+        else{
+            document.getElementById("id_surnames").className='input';
+            if(user === ""){
+                document.getElementById("id_username").className='error';
+                document.getElementById("id_username").value="";
+                return false;
+            }
+            else{
+
+                document.getElementById("id_username").className='input';
+                if(email === "" || !emailexpression.test(email)){
+                    document.getElementById("id_email").className='error';
+                    document.getElementById("id_email").value  = "";
+                    return false;
+                }
+
+                else{
+                    document.getElementById("id_email").className='input';
+                    if(password1 === "" || password2 === "" || password1 !== password2){
+                        document.getElementById("id_password").className='error';
+                        document.getElementById("id_password").value="";
+                        document.getElementById("id_confirm").className='error';
+                        document.getElementById("id_confirm").value="";
+                        return false;
+                    }
+                    else{
+                        document.getElementById("id_password").className='input';
+                        document.getElementById("id_confirm").className='input';
+                        return true;
+                    }
+                }
+            }
+
+        }
     }
-    else if(correo === "" || !expresionemail.test(correo)){
-        document.getElementById("correo").className='error';
-        document.getElementById("correo").value  = "";
-        return false;
-    }
-    else if(usuario === ""){
-        document.getElementById("usuario").className='error';
-        document.getElementById("usuario").value="";
-        return false;
-    }
-
-    else
-        return true;
-
-}
-function Registro(){
-
-    console.log(validar()); //comprobamos si todos los datos son correctos
-
 }
