f3f6654dd8206f8bcb483390afbb84bbd87250b8
Maria Victoria Alba <mariavictoria.albacarav@alum.uca.es>
2021-01-08 22:38:48 +0100

    Registry form

---
+++ betsite/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Student
+
+class RegisterForm(forms.Form):
+
+    name = forms.CharField(label="",max_length=20, widget=forms.TextInput(attrs={'class': 'input','placeholder':"Nombre"}))
+    surnames = forms.CharField(label="",max_length=40, widget=forms.TextInput(attrs={'class': 'input','placeholder':"Apellidos"}))
+    username = forms.CharField(label="",max_length=20, widget=forms.TextInput(attrs={'class': 'input','placeholder': 'Usuario'}))
+    email = forms.CharField(label="", widget=forms.TextInput(attrs={'class': 'input','placeholder':"Email"}))
+    password = forms.CharField(max_length=20,label="", widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder':"Contraseña"}))
+    confirm = forms.CharField(max_length=20, label="", widget=forms.PasswordInput(attrs={'class': 'input', 'placeholder':"Repite contraseña"}))
+
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        email = self.cleaned_data.get("email")
+        name = self.cleaned_data.get("name")
+        surnames = self.cleaned_data.get("surnames")
+        password = self.cleaned_data.get("password")
+
+        if Student.objects.filter(email=email).exists():
+            self.add_error("email","El email no es único")
+        else:
+            if Student.objects.filter(username=username).exists():
+                self.add_error("username", "El usuario no es único")
+        values = {
+            "email": email,
+            "username": username,
+            "name": name,
+            "surnames": surnames,
+            "password": password,
+        }
+        return values
+
+
