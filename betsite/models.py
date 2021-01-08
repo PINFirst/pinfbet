d871d3c2e3c862a25e867fe12fcf7be54ba18c4f
Maria Victoria Alba <mariavictoria.albacarav@alum.uca.es>
2021-01-08 22:40:51 +0100

    student model

--- betsite/models.py
+++ betsite/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Student(User):
+    hide_email = models.BooleanField(default=True)
+
+
