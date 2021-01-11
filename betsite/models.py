01b9f9fd3fb8324cd01e39141c7a415f759d3195
SergioDR <sergio.diazra@alum.uca.es>
2021-01-11 11:42:35 +0100

    refactor: update Student fields

--- betsite/models.py
+++ betsite/models.py
@@ -19,15 +19,13 @@ class Subject(models.Model):
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30, verbose_name='Nombre')
-    dni =  models.CharField(max_length=9, verbose_name='DNI')
     image = models.ImageField(default='default.jpg', upload_to='profile_pics', verbose_name='Imagen de perfil')
     coins = models.FloatField(default=0)
     current_subjects = models.ManyToManyField(Subject, verbose_name='Asignaturas matriculadas')
-    passed_subjects = models.ManyToManyField(Subject, null=True, blank=True, verbose_name='Asignaturas aprobadas', related_name='passed_subjects')
+    passed_subjects = models.ManyToManyField(Subject, blank=True, verbose_name='Asignaturas aprobadas', related_name='passed_subjects')
 
     def __str__(self):
-        return self.name + ' ' + self.user.first_name + ' ' + self.user.last_name + ' (' + self.user.username + ')'
+        return self.user.first_name + ' ' + self.user.last_name + ' (' + self.user.username + ')'
 
     class Meta:
         verbose_name = 'Alumno'
@@ -37,8 +35,10 @@ class Bet(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Alumno')
     friend = models.ForeignKey(Student, on_delete=models.CASCADE, blank=True, null=True, related_name='friend', verbose_name='Amigo apostado')
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, verbose_name='Asignatura')
-    grade = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)], verbose_name='Nota apostada')
+    bet_grade = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)], verbose_name='Nota apostada')
+    actual_grade = models.IntegerField(null=True, blank=True, validators=[MaxValueValidator(10), MinValueValidator(0)], verbose_name='Nota sacada')
     coins = models.FloatField()
+    paid = models.BooleanField(default=False)
     start_date = models.DateTimeField(auto_now_add=True, verbose_name='Fecha apuesta')
     end_date = models.DateTimeField(verbose_name='Fecha fin de apuesta')
 
